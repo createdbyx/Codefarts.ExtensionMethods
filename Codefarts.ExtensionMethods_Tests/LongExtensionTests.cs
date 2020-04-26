f5f5fd9328f2e595e493d642a63e7fe951a06022
Dean <deanlunz@createdbyx.com>
2020-04-26 16:13:35 -0700

    added unit tests

---
+++ Codefarts.ExtensionMethods_Tests/LongExtensionTests.cs
@@ -0,0 +1,40 @@
+﻿namespace Codefarts.Tests.ExtensionMethods
+{
+    using System;
+
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass, TestCategory("Base Types")]
+    public class LongExtensionTests
+    {
+        [TestMethod]
+        public void IsInRange_Number_In_Range()
+        {
+            Assert.IsTrue(123L.IsInRange(0, 500));
+        }
+
+        [TestMethod]
+        public void IsInRange_Number_Below_Range()
+        {
+            Assert.IsFalse(123L.IsInRange(400, 500));
+        }
+
+        [TestMethod]
+        public void IsInRange_Number_Above_Range()
+        {
+            Assert.IsFalse(123L.IsInRange(0, 100));
+        }
+
+        [TestMethod]
+        public void IsInRange_Number_At_Min_Range()
+        {
+            Assert.IsTrue(123L.IsInRange(123, 500));
+        }
+
+        [TestMethod]
+        public void IsInRange_Number_At_Max_Range()
+        {
+            Assert.IsTrue(123L.IsInRange(0, 123));
+        }
+    }
+}
